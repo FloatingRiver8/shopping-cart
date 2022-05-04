b8fe4f6d3045d2e0c3e71f0e3b996f0c329a20e4
FloatingRiver8 <maceiramariana@gmail.com>
2022-05-03 21:07:42 -0300

    adding Navbar and intemListContainer components plus cart

---
+++ src/Components/CartWidget.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
+
+function CartWidget() {
+    return (
+        <>
+            <svgIcon>
+                <AddShoppingCartRoundedIcon />
+            </svgIcon>
+
+        </>
+    )
+}
+
+export default CartWidget
---
+++ src/Components/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ItemListContainer() {
+
+
+
+  return (
+
+    <div>
+      <h2>Aquí irá mi catálogo de productos</h2>
+    </div>
+
+
+  )
+}
